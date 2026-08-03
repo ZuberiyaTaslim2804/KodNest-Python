efc8734b6c96837dd5612ad22ab7cb099b9248c4
ZUBERIYA TASLIM KHATEEB <zuberiyataslim@gmail.com>
2026-08-03 17:19:44 +0530

    Module2

---
+++ track/M02-basics-of-python/T01-variable-dynamically-typed/ST01-variable-type/src/variable_type.py
@@ -0,0 +1,10 @@
+# Create the variables required
+student_name="Aarav"
+course_duration=30
+course_rating=4.8
+is_enrolled=True
+# Display the type of variables
+print(type(student_name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
