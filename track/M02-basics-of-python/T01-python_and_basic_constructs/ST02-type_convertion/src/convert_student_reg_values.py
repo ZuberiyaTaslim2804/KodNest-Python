844d3cf3c79aab96d4e29cd807ba16602faf37b6
ZUBERIYA TASLIM KHATEEB <zuberiyataslim@gmail.com>
2026-08-05 12:32:13 +0530

    Day4

---
+++ track/M02-basics-of-python/T01-python_and_basic_constructs/ST02-type_convertion/src/convert_student_reg_values.py
@@ -0,0 +1,18 @@
+age_text="21"
+course_fee_text="499.50"
+attempt=3
+
+#convet the values here
+age=int(age_text)
+course_fee=float(course_fee_text)
+attempts_text=str(attempt)
+
+# Display the converted values and their data type
+print(age)
+print(type(age))
+
+print(course_fee)
+print(type(course_fee))
+
+print(attempts_text)
+print(type(attempts_text))
---
+++ track/M02-basics-of-python/T01-python_and_basic_constructs/ST04-comparison-and-logical-operators/src/calculate-division-and-floorDivision.py
@@ -0,0 +1,7 @@
+# Read the values
+first_number=int(input())
+second_number=int(input())
+
+# find the dividon and floor-division
+print(first_number/second_number)
+print(first_number//second_number)
