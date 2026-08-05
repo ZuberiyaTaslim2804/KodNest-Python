354c193f01153c4a9ca08a985fb14b220c9c6272
ZUBERIYA TASLIM KHATEEB <zuberiyataslim@gmail.com>
2026-08-05 17:12:56 +0530

    Day5 changes

---
+++ track/M02-Introduction-of-python/T02-Conditional-Statement-and-loops/ST04-for-loop/number_seq_and_word_char.py
@@ -0,0 +1,13 @@
+# read the n and word value
+n=int(input())
+word=input()
+
+#Display the 1 to n numbers
+print("Numbers:")
+for i in range(1,n+1):
+    print(i)
+
+# Display the characters of word
+print("Characters:")
+for i in word:
+    print(i)
