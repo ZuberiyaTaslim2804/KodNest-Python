9b14433ccbd1810287d65644871d15f4d84f8eb4
ZUBERIYA TASLIM KHATEEB <zuberiyataslim@gmail.com>
2026-08-11 17:24:28 +0530

    Mutable Default Arguments

---
+++ track/M02-Introduction-of-python/T04-functions_dictionary_sets/ST02-Arguments/mutable_deafult_argu.py
@@ -0,0 +1,24 @@
+# using Mutable Default Argument
+def add_student(name,students=[]):
+    students.append(name)
+    print(students)
+
+first=input()
+second=input()
+third=input()
+
+print("Using Mutable Deafult Argument")
+add_student(first)
+add_student(second)
+add_student(third)
+# Using None as Default Argument
+def add_task(task,tasks=None):
+    if tasks is None:
+        tasks = []
+    
+    tasks.append(task)
+    return task
+
+print("Deafult argument -None")
+print(add_task("Learn Python"))
+print(add_task("Paractice Functions"))
