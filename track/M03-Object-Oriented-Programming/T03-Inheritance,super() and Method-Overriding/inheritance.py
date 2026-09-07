bad280800d5a4a466bc18287376676a522ef5596
ZUBERIYA TASLIM KHATEEB <zuberiyataslim@gmail.com>
2026-09-07 15:20:56 +0530

    Inheriting the method from parent to class class

---
+++ track/M03-Object-Oriented-Programming/T03-Inheritance,super() and Method-Overriding/inheritance.py
@@ -0,0 +1,16 @@
+class Person:
+    # Create display_name() here
+
+    def display_name(self, name):
+        print(f"Student Name: {name}")
+
+
+class Student(Person):
+    pass
+
+
+name = input().strip()
+
+# Create a Student object and call display_name()
+s = Student()
+s.display_name(name)
